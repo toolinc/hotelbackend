8c420af88a335f089ab830bbb8c70754baa0da06
jovanimtzrico@github.com <jovanimtzrico@gmail.com>
2017-12-07 15:30:47 -0800

    Adding c3p0 as the database pool.

--- src/main/java/com/toolsoft/api/HotelInfo.java
+++ src/main/java/com/toolsoft/api/HotelInfo.java
@@ -1,19 +1,42 @@
 package com.toolsoft.api;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 @Singleton
 public class HotelInfo extends HttpServlet {
 
+  @Inject
+  private DataSource dataSource;
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
+      throws IOException, ServletException {
     response.setContentType("application/json");
     response.setHeader("Access-Control-Allow-Origin", "*");
+
+    final String selectSql = "SELECT * FROM Hotels";
+    PrintWriter out = response.getWriter();
+    try (ResultSet rs = dataSource.getConnection().prepareStatement(selectSql).executeQuery()) {
+      while (rs.next()) {
+        String name = rs.getString("name");
+        String city = rs.getString("city");
+        out.print("Name: " + name + " city: " + city + "\n");
+      }
+    } catch (SQLException e) {
+      throw new ServletException("SQL error", e);
+    }
+
     response.getWriter().println("[{\n"
         + "\"success\":true,\n"
         + "\"hotelId\":\"1\",\n"
--- src/main/java/com/toolsoft/inject/HotelModule.java
+++ src/main/java/com/toolsoft/inject/HotelModule.java
@@ -1,14 +1,33 @@
 package com.toolsoft.inject;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+import javax.sql.DataSource;
 
 /**
  * Specifies the required instances that need to be injected into the classes.
  */
 public class HotelModule extends AbstractModule {
 
+  private static final String DRIVER = "com.mysql.jdbc.Driver";
+  private static final String URL = "jdbc:mysql://google/HotelAdvisor?cloudSqlInstance=hotelbackend:us-central1:hoteldb&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=root&password=98329973&useSSL=false";
+
   @Override
   protected void configure() {
+  }
 
+  @Provides
+  DataSource providesPooledDataSource() throws IOException, PropertyVetoException {
+    ComboPooledDataSource cpds = new ComboPooledDataSource();
+    cpds.setDriverClass(DRIVER);
+    cpds.setJdbcUrl(URL);
+    cpds.setMinPoolSize(2);
+    cpds.setAcquireIncrement(1);
+    cpds.setMaxPoolSize(8);
+    cpds.setMaxStatements(100);
+    return cpds;
   }
 }
