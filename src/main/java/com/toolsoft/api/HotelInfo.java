a34b7e0c4a25cba6a0b3b14655c204bc18a5ac54
jovanimtzrico@github.com <jovanimtzrico@gmail.com>
2017-12-12 22:50:29 -0800

    adding a share json utility method

--- src/main/java/com/toolsoft/api/HotelInfo.java
+++ src/main/java/com/toolsoft/api/HotelInfo.java
@@ -1,13 +1,13 @@
 package com.toolsoft.api;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.toolsoft.api.ResponseUtil.hotelsToJson;
 import static com.toolsoft.api.ResponseUtil.setCors;
 import static com.toolsoft.api.ResponseUtil.setJsonContentType;
 
 import com.google.common.base.Strings;
 import com.google.gson.Gson;
 import com.toolsoft.dao.HotelDao;
-import com.toolsoft.model.Address;
 import com.toolsoft.model.Hotel;
 import java.io.IOException;
 import java.util.List;
@@ -40,33 +40,9 @@ public final class HotelInfo extends HttpServlet {
     setCors(response);
     setJsonContentType(response);
     if (Strings.isNullOrEmpty(version)) {
-      response.getWriter().print(oldJson(hotels));
+      response.getWriter().print(hotelsToJson(hotels));
     } else {
       response.getWriter().print(new Gson().toJson(hotels));
     }
-  }
-
-  public String oldJson(List<Hotel> hotels) {
-    StringBuilder sb = new StringBuilder("[");
-    for (Hotel hotel : hotels) {
-      sb.append("{" + System.lineSeparator() + "\"success\":");
-      Address address = hotel.address();
-      sb.append("true," + System.lineSeparator());
-      sb.append("\"hotelId\":\"" + hotel.id() + "\"," + System.lineSeparator());
-      sb.append("\"name\":\"" + hotel.name() + "\"," + System.lineSeparator());
-      sb.append("\"image\":\"" + hotel.image() + "\"," + System.lineSeparator());
-      sb.append("\"link\":\"" + hotel.link() + "\"," + System.lineSeparator());
-      sb.append("\"rating\":\"" + hotel.rating() + "\"," + System.lineSeparator());
-      sb.append("\"addr\":\"" + address.streetAddress() + "\"," + System.lineSeparator());
-      sb.append("\"city\":\"" + address.city() + "\"," + System.lineSeparator());
-      sb.append("\"state\":\"" + address.state() + "\"," + System.lineSeparator());
-      sb.append("\"lat\":\"" + address.lat() + "\"," + System.lineSeparator());
-      sb.append("\"lng\":\"" + address.lon() + "\"" + System.lineSeparator());
-      sb.append("},");
-    }
-    if (sb.length() > 1) {
-      sb.deleteCharAt(sb.lastIndexOf(","));
-    }
-    return sb.append("]").toString();
   }
 }
--- src/main/java/com/toolsoft/api/ResponseUtil.java
+++ src/main/java/com/toolsoft/api/ResponseUtil.java
@@ -1,5 +1,8 @@
 package com.toolsoft.api;
 
+import com.toolsoft.model.Address;
+import com.toolsoft.model.Hotel;
+import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 final class ResponseUtil {
@@ -19,5 +22,29 @@ final class ResponseUtil {
    */
   static final void setCors(HttpServletResponse response) {
     response.setHeader(CORS, CORS_VALUE);
+  }
+
+  static final String hotelsToJson(List<Hotel> hotels) {
+    StringBuilder sb = new StringBuilder("[");
+    for (Hotel hotel : hotels) {
+      sb.append("{" + System.lineSeparator() + "\"success\":");
+      Address address = hotel.address();
+      sb.append("true," + System.lineSeparator());
+      sb.append("\"hotelId\":\"" + hotel.id() + "\"," + System.lineSeparator());
+      sb.append("\"name\":\"" + hotel.name() + "\"," + System.lineSeparator());
+      sb.append("\"image\":\"" + hotel.image() + "\"," + System.lineSeparator());
+      sb.append("\"link\":\"" + hotel.link() + "\"," + System.lineSeparator());
+      sb.append("\"rating\":\"" + hotel.rating() + "\"," + System.lineSeparator());
+      sb.append("\"addr\":\"" + address.streetAddress() + "\"," + System.lineSeparator());
+      sb.append("\"city\":\"" + address.city() + "\"," + System.lineSeparator());
+      sb.append("\"state\":\"" + address.state() + "\"," + System.lineSeparator());
+      sb.append("\"lat\":\"" + address.lat() + "\"," + System.lineSeparator());
+      sb.append("\"lng\":\"" + address.lon() + "\"" + System.lineSeparator());
+      sb.append("},");
+    }
+    if (sb.length() > 1) {
+      sb.deleteCharAt(sb.lastIndexOf(","));
+    }
+    return sb.append("]").toString();
   }
 }
